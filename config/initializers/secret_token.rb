16153d045103910d56184dbf8da87bb7ffdd33ff
sidra.bhatti@vibes.com <sidra.bhatti@vibes.com>
2013-06-06 10:23:22 -0500

    edit gitignore file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footsteps::Application.config.secret_token = '0a893b7a42ff4546f323fe7484caff0120f6c711cfa635c0ff8503c9e8785da8ffa64886a16453c64a9e5a5514a720dca4efc3dad8e3bdb09f51ece691b104fc'
